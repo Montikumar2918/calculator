f7dbcb510196bc473e79321cf90c8f970c4fe12c
Monti kumar <kumarmonti445@gmail.com>
2021-08-16 15:45:03 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,25 @@
+function c(val)
+{
+document.getElementById("d").value=val;
+}
+function v(val)
+{
+document.getElementById("d").value+=val;
+}
+function back()
+{
+  var value = document.getElementById("d").value;
+  document.getElementById("d").value=value.substr(0, value.length - 1);
+}
+ 
+function e() 
+{ 
+try 
+{ 
+  c(eval(document.getElementById("d").value)) 
+} 
+catch(e) 
+{
+  alert('invalid input'); 
+} 
+}  
